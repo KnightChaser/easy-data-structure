da42db8f0f961fe322e73adc93c97309f15b286d
Lee Garam <agerio100@naver.com>
2024-03-21 21:38:58 +0900

    Update stack_basic.cpp
    Convert that into C++ish style

--- stack/stack_basic.cpp
+++ stack/stack_basic.cpp
@@ -2,9 +2,10 @@
  * @file stack_basic.cpp
  * @brief An implementation of basic stack(stack_basic.c) in C++, an integer stack.
 */
+
 #include <iostream>
-#include <cstring>
 #include <stdexcept>
+
 #define MAX_STACK_SIZE 5
 
 class Stack {
@@ -12,22 +13,21 @@ private:
     int stackTopIndex;
     int stack[MAX_STACK_SIZE];
 public:
-    void initialization();
-    bool isStackEmpty();
-    bool isStackFull();
+    // Constructor, initializes the stack
+    Stack() {
+        std::cout << "Stack initialized with max size of " << MAX_STACK_SIZE << std::endl;
+        this->stackTopIndex = -1;
+    }
+
+    bool isStackEmpty() const;
+    bool isStackFull() const;
     void push(int data);
     int pop();
-    int peek();
+    int peek() const;
 };
 
-// Stack initialization
-void Stack::initialization() {
-    this->stackTopIndex = -1;
-    memset(this->stack, 0, sizeof(this->stack));
-}
-
 // Check if the stack is empty
-bool Stack::isStackEmpty() {
+bool Stack::isStackEmpty() const {
     if (this->stackTopIndex == -1)
         return true;
     else
@@ -35,7 +35,7 @@ bool Stack::isStackEmpty() {
 }
 
 // Check if the stack is full
-bool Stack::isStackFull() {
+bool Stack::isStackFull() const {
     if (this->stackTopIndex == (MAX_STACK_SIZE - 1))
         return true;
     else
@@ -44,52 +44,48 @@ bool Stack::isStackFull() {
 
 // Push data into the stack
 void Stack::push(int data) {
-    if (this->isStackFull()) {
+    if (isStackFull()) {
         std::cout << "Stack is full" << std::endl;
         throw std::overflow_error("Stack is full, so can't push data into it.");
     }
     this->stackTopIndex += 1;
-    this->stack[this->stackTopIndex] = data;
+    stack[this->stackTopIndex] = data;
 }
 
 // Pop data from the stack
 int Stack::pop() {
-    if (this->isStackEmpty()) {
+    if (isStackEmpty()) {
         std::cout << "Stack is empty" << std::endl;
         throw std::underflow_error("Stack is empty, so can't pop data from it.");
     } 
-    int data = this->stack[this->stackTopIndex];
+    int data = stack[stackTopIndex];
     this->stackTopIndex -= 1;
     return data;
 }
 
-// Peek the top of the stack, without removing it
-int Stack::peek() {
-    if (this->isStackEmpty()) {
+// Peek data from the stack
+int Stack::peek() const {
+    if (isStackEmpty()) {
         std::cout << "Stack is empty" << std::endl;
         throw std::underflow_error("Stack is empty, so can't peek data from it.");
     }
-    return this->stack[this->stackTopIndex];
+    return stack[stackTopIndex];
 }
 
 int main(void) {
-
-    // Stack intialization and pushing
     Stack stack;
-    stack.initialization();
-    stack.push(1);
-    stack.push(2);
-    stack.push(3);
-    stack.push(4);
-    stack.push(5);
 
-    // Peeking and popping data from the stack.
     try {
-        for (int i = 0; i < MAX_STACK_SIZE + 1; i++) {
+        for (int index = 1; index <= MAX_STACK_SIZE; index++) {
+            std::cout << "pushing: " << index << std::endl;
+            stack.push(index);
+        }
+
+        for (int index = 0; index < MAX_STACK_SIZE + 1; index++) {
             std::cout << "peeking: " << stack.peek() << std::endl;
             std::cout << "popping: " << stack.pop() << std::endl;
         }
-    } catch (std::exception& exception) {
+    } catch (const std::exception& exception) {
         std::cout << "Exception during handling the stack: " << exception.what() << std::endl;
     }
 
