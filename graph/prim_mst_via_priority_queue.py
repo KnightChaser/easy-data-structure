1f43e383c477579eeec8cdb1b4cf3a5e30239ec0
Lee Garam <agerio100@naver.com>
2024-06-05 12:35:07 +0900

    Changed graph vertex index format(int->str)

--- graph/prim_mst_via_priority_queue.py
+++ graph/prim_mst_via_priority_queue.py
@@ -1,28 +1,30 @@
 # @file: prim_mst_via_priority_queue.py
-# @brief: Prim's Minimum Spanning Tree Algorithm implementation in Python using priority queue to represent the graph
-#         Since the grpah uses priority queue, the time complexity of the algorithm is O(E + VlogV) where V is the number of vertices in the graph
-#         and E is the number of edges in the graph
+# @brief: Prim's Minimum Spanning Tree Algorithm implementation in Python using a priority queue.
+#         Since the graph uses a priority queue, the time complexity of the algorithm is O(E + VlogV) where V is the number of vertices in the graph
+#         and E is the number of edges in the graph.
 
 from typing import List, Tuple, Dict
 import heapq
 
-def prim_mst_via_priority_queue(graph_adjacency_list) -> Tuple[List[Tuple[int, int, int]], int]:
+def prim_mst_via_priority_queue(graph_adjacency_list: Dict[str, List[Tuple[str, int]]]) -> Tuple[List[Tuple[str, str, int]], int]:
+
     # Select an arbitrary vertex as the starting vertex
-    start_vertex: int = next(iter(graph_adjacency_list))
+    start_vertex: str = next(iter(graph_adjacency_list))
 
     # Initialize the priority queue with the starting vertex
-    priority_queue: List[Tuple[int, int]] = []
+    priority_queue: List[Tuple[int, str, str]] = []
 
     # Keep track of the vertices that are already visited
-    visited_vertices: List[bool] = [False] * len(graph_adjacency_list)
+    visited_vertices: Dict[str, bool] = {vertex: False for vertex in graph_adjacency_list}
 
     # Keep track of the edges in the MST (The edges that connect the vertices in the MST)
-    mst_edges: List[Tuple[int, int]] = []
+    mst_edges: List[Tuple[str, str, int]] = []
 
     # Keep track of the total weight of the MST
     total_mst_weight: int = 0
 
     # Add the edges of the starting vertex to the priority queue
+    # Note that heapq uses a min-heap and sort by the first element of the tuple
     visited_vertices[start_vertex] = True
     for neighbour_vertex, edge_weight in graph_adjacency_list[start_vertex]:
         heapq.heappush(priority_queue, (edge_weight, start_vertex, neighbour_vertex))
@@ -48,14 +50,14 @@ def prim_mst_via_priority_queue(graph_adjacency_list) -> Tuple[List[Tuple[int, i
     return mst_edges, total_mst_weight
 
 if __name__ == "__main__":
-    # Graph is represented as adjacency list
+    # Graph is represented as an adjacency list
     # graph[node] = [(neighbour, weight), ...]   (A node has a list of neighbours with their corresponding edge weights)
-    graph_adjacency_list: Dict[int, List[Tuple[int, int]]] = {
-        0: [(1, 2), (3, 6)],
-        1: [(0, 2), (2, 3), (3, 8), (4, 5)],
-        2: [(1, 3), (4, 7)],
-        3: [(0, 6), (1, 8), (4, 9)],
-        4: [(1, 5), (2, 7), (3, 9)]
+    graph_adjacency_list: Dict[str, List[Tuple[str, int]]] = {
+        'A': [('B', 2), ('D', 6)],
+        'B': [('A', 2), ('C', 3), ('D', 8), ('E', 5)],
+        'C': [('B', 3), ('E', 7)],
+        'D': [('A', 6), ('B', 8), ('E', 9)],
+        'E': [('B', 5), ('C', 7), ('D', 9)]
     }
 
     mst_edges, total_mst_weight = prim_mst_via_priority_queue(graph_adjacency_list)
@@ -65,8 +67,8 @@ if __name__ == "__main__":
     print(f"Total Weight of the MST: {total_mst_weight}")
 
 # Minimum Spanning Tree Edges:
-# (Vertex 0 - Vertex 1) with weight 2
-# (Vertex 1 - Vertex 2) with weight 3
-# (Vertex 1 - Vertex 4) with weight 5
-# (Vertex 0 - Vertex 3) with weight 6
+# (Vertex A - Vertex B) with weight 2
+# (Vertex B - Vertex C) with weight 3
+# (Vertex B - Vertex E) with weight 5
+# (Vertex A - Vertex D) with weight 6
 # Total Weight of the MST: 16
